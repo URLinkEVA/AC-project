ae39a983a64175fa96381a5764a2ece7d77f530a
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-05-08 14:00:05 +0800

    Create 50_2.cpp

---
+++ ACWweekly/50/50_2.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+const int INF = 1e9;
+
+int main()
+{
+	int n, m;
+	scanf("%d", &n);
+
+	int a = INF, b = -INF;
+	while (n--)
+	{
+		int l, r;
+		scanf("%d%d",&l,&r);
+		a = min(a, r);
+		b = max(b, r);
+	}
+	scanf("%d", &m);
+	
+	int res = 0;
+	while (m--)
+	{
+		int l, r;
+		scanf("%d%d",&l,&r);
+		if (b > a) res = max(res, b - r);
+		if (a < l) res = max(res, l - a);
+	}
+
+	printf("%d\n", res);
+	return 0;
+}
