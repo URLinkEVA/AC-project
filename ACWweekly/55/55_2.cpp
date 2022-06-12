2801a9bd0d36fb7acc6c61b6d03a093e106274d1
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-06-12 15:58:21 +0800

    Create 55_2.cpp

---
+++ ACWweekly/55/55_2.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+typedef long long LL;
+
+const int N = 200010, INF = 2e9;
+
+int c[N], a[N], b[N];
+int n, m;
+int ans[N];
+
+int main()
+{
+	scanf("%d%d", &n, &m);
+	for (int i = 0; i < n+m; i++) scanf("%d",&c[i]);
+	for (int i = 0, j = 0, k = 0; i < n + m; i++)
+	{
+		int t;
+		scanf("%d", &t);
+		if (t) b[++j] = c[i];
+		else a[++k] = c[i];
+	}
+
+	b[0] = -INF, b[m+1] = INF;
+
+	for (int i = 1; i <= n; i++)
+	{
+		int l = 0, r = m + 1;
+		while (l < r)
+		{
+			int mid = l + r >> 1;
+			if (b[mid] >= a[i]) r = mid;
+			else l = mid + 1;
+		}
+		if ((LL)a[i] - b[r - 1] <= (LL)b[r] - a[i]) ans[r - 1]++;
+		else ans[r]++;
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		printf("%d ", ans[i]);
+	}
+	return 0;
+}
