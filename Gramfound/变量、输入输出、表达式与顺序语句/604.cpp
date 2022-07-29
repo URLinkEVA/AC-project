65996218d9721664a9da14073d5fa8432b26c365
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-07-29 12:53:41 +0800

    Create 604.cpp

---
+++ Gramfound/变量、输入输出、表达式与顺序语句/604.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    double R,pi = 3.14159;
+    cin>>R;//scanf("%lf\n",&R)
+    printf("A=%.4lf\n", pi * R * R);
+    return 0;
+}
