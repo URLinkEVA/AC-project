67c56efe3d4049d993f14420a21ce10416383ee3
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-07-29 12:54:15 +0800

    Create 606.cpp

---
+++ Gramfound/变量、输入输出、表达式与顺序语句/606.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    double A,B;
+    cin >> A >> B;
+    printf("MEDIA = %.5lf", (3.5*A+7.5*B)/11);
+    return 0;
+}
