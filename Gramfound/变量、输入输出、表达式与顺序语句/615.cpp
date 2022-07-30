b724c7450beb895600b75ee0b4adac1d0dd9a0e4
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-07-30 10:36:35 +0800

    Create 615.cpp

---
+++ Gramfound/变量、输入输出、表达式与顺序语句/615.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int a;
+    double b;
+    cin >> a >> b;
+    printf("%.3lf km/l", a/b);
+    return 0;
+}
