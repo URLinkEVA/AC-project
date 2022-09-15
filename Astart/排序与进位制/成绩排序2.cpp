1358343002bcaf879e623cb44fb4431e10abc9bc
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-09-15 22:21:38 +0800

    Create 成绩排序2.cpp

---
+++ Astart/排序与进位制/成绩排序2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+#include<algorithm>
+
+using namespace std;
+
+const int N = 110;
+
+int n;
+struct Person{
+    int id;
+    int score;
+    bool operator<(const Person&t) const
+    {
+        if (score!=t.score) return score < t.score;
+        return id < t.id;
+    }
+}q[N];
+
+int main(){
+    cin >> n;
+    for (int i = 0; i < n; i ++ )
+        cin >> q[i].id >> q[i].score;
+    sort(q, q+n);
+
+    for (int i = 0; i < n; i ++ )
+        cout << q[i].id << ' ' << q[i].score << endl;
+        
+    return 0;
+}
