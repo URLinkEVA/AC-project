d8fa4ba7f7a607b409d87bb93fd4b812e8f8e57d
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-09-24 21:47:38 +0800

    Create 两个素数.cpp

---
+++ ACWweekly/70/两个素数.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int is_prime(int n)
+{
+    for (int i = 2; i <= n / 2; i ++ )
+    {
+        if(n % i == 0) return 0;
+    }
+    return 1; // 返回代表for循环里判断数为素数
+}
+
+int main()
+{
+    int x;
+    cin >> x;
+    for (int i = 2; i <= x / 2; i ++ )
+    {
+        for (int j = 2; j <= x / 2; j ++ )
+        {
+            if(is_prime(i) && is_prime(j) && i * j == x && i <= j) cout << i << ' ' << j << endl;
+        }
+    }
+    return 0;
+}
