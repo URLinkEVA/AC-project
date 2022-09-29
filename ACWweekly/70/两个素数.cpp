539619c23f2ef4e23d30a0b673ab6a7228e86819
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-09-29 22:07:20 +0800

    Update 两个素数.cpp

--- ACWweekly/70/两个素数.cpp
+++ ACWweekly/70/两个素数.cpp
@@ -26,3 +26,27 @@ int main()
     }
     return 0;
 }
+
+
+// 更快的写法
+
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cin >> x;
+    for (int i = 2; i < x; i ++ )
+    {
+        if(x % i == 0)
+        {
+            cout << i << ' ' << x/i << endl;
+            break;
+        }
+    }
+    return 0;
+}
