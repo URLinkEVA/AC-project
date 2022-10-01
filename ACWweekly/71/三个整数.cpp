5053fc6f1942f22a08db873f3ed9d9ee0e37b53e
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-10-01 23:59:04 +0800

    Create 三个整数.cpp

---
+++ ACWweekly/71/三个整数.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << b << ' ' << c << ' ' << c << ' ' << endl;
+    return 0;
+}
