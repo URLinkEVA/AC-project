67a5bbaa2512d35620d3ea8ef678d1c961849c26
kongla <52813199+URLinkEVA@users.noreply.github.com>
2022-11-06 16:32:57 +0800

    Update 排列数字.cpp

--- AlgoBasic/搜索与图论/DFS/排列数字.cpp
+++ AlgoBasic/搜索与图论/DFS/排列数字.cpp
@@ -7,23 +7,26 @@ using namespace std;
 const int N = 10;
 int n;
 int path[N];
+bool st[N];
 
-void dfs(int u, int state)
+void dfs(int u)
 {
     if(u == n)
     {
         for (int i = 0; i < n; i ++ ) cout << path[i] << ' ';
-        puts("");
+        puts(""); // 每个方案后输出一个空行
         return; 
     }
 
     else
     {
-        for (int i = 0; i < n; i ++ )
-            if(!(state >> i & 1))
+        for (int i = 1; i <= n; i ++ )
+            if(!st[i])
             {
-                path[u] = i + 1;
-                dfs(u + 1, state + (1 << i));
+                path[u] = i;
+                st[i] = true;
+                dfs(u + 1);
+                st[i] = false;
             }
     }
 }
@@ -31,6 +34,6 @@ void dfs(int u, int state)
 int main()
 {
     cin >> n;
-    dfs(0, 0);
+    dfs(0);
     return 0;
 }
