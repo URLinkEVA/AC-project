be2cac9a4d8bcf5b70834faf4b424ad4adb715f2
kongla <52813199+URLinkEVA@users.noreply.github.com>
2023-02-11 21:08:07 +0800

    Create 首字母大写.cpp

---
+++ ACWweekly/90/首字母大写.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    if(s[0]>='a' && s[0]<='z') s[0]-=32;
+    cout << s;
+    return 0;
+}
