a8129ebc9a4a8d5376024263a44c7bcd8aea935d
kongla <52813199+URLinkEVA@users.noreply.github.com>
2023-03-12 18:20:42 +0800

    Create 装物品.cpp

---
+++ ACWweekly/94/装物品.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    // if(n%5==0) cout << n/5;
+    // else cout << (n/5)+1;
+    cout << (n+4)/5;
+    return 0;
+}
