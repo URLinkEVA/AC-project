038b6ace86b8cfb845b65fd4d725fcb11f962d2b
kongla <52813199+URLinkEVA@users.noreply.github.com>
2023-03-26 21:32:11 +0800

    Create 完美数.cpp

---
+++ ACWweekly/96/完美数.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+typedef long long LL;
+
+int main()
+{
+    LL n;
+    cin >> n;
+    cout << n/2520;
+    return 0;
+}
