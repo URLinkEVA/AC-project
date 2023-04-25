e20842218116046293348ff0a8222f0375da3a0a
kongla <52813199+URLinkEVA@users.noreply.github.com>
2023-04-25 13:21:27 +0800

    Create 比赛.cpp

---
+++ ACWweekly/100/比赛.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n, b, p;
+    cin >> n >> b >> p;
+    cout << (n-1)*(2*b+1) << ' ' << n*p << endl;
+    return 0;
+}
